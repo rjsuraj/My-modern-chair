e4553ebc5f291f013b1d63b561fa2dc09c4aad73
suraj mandal <rjsurajmandal@gmail.com>
2023-02-03 22:47:34 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //changing color of background and cart-btn
 
-// color6
-// linear-gradient(196deg, #aaadac, #838786);
+
 
 
 const color1 = document.getElementById("color-1");
@@ -13,14 +12,35 @@ const color6 = document.getElementById("color-6");
 
 const background = document.getElementById("background");
 const cartButton = document.getElementById("cartButton");
+const chair = document.getElementById("chair");
 
 
+
+function startanimation(element) {
+
+    element.classList.add("shakeEffect");
+
+    element.addEventListener( "animationend",  function() {
+
+      element.classList.remove("shakeEffect");  	
+
+    } );
+
+}
 
 
 
 color1.addEventListener('click',()=>{
     color1.classList.add("selected");
+    color2.classList.remove("selected");
+    color3.classList.remove("selected");
+    color4.classList.remove("selected");
+    color5.classList.remove("selected");
+    color6.classList.remove("selected");
     cartButton.style.background = "#944852";
+
+    chair.src = "assets/chair-1.png";
+    chair.addEventListener("click", startanimation(chair));
 
     let opacity = window.getComputedStyle(
         document.querySelector('#background'), ':before'
@@ -45,7 +65,15 @@ color1.addEventListener('click',()=>{
 
 color2.addEventListener('click',()=>{
     color2.classList.add("selected");
+    color1.classList.remove("selected");
+    color3.classList.remove("selected");
+    color4.classList.remove("selected");
+    color5.classList.remove("selected");
+    color6.classList.remove("selected");
     cartButton.style.background = "#1a1a1a";
+
+    chair.src = "assets/chair-2.png";
+    chair.addEventListener("click", startanimation(chair));
 
     let opacity = window.getComputedStyle(
         document.querySelector('#background'), ':before'
@@ -68,9 +96,50 @@ color2.addEventListener('click',()=>{
 
 })
 
+color3.addEventListener('click',()=>{
+    color3.classList.add("selected");
+    color1.classList.remove("selected");
+    color2.classList.remove("selected");
+    color4.classList.remove("selected");
+    color5.classList.remove("selected");
+    color6.classList.remove("selected");
+    cartButton.style.background = "#40566e";
+
+    chair.src = "assets/chair-3.png";
+    chair.addEventListener("click", startanimation(chair));
+
+    let opacity = window.getComputedStyle(
+        document.querySelector('#background'), ':before'
+    ).getPropertyValue('opacity');
+    
+    // console.log(opacity)    
+
+    if(opacity == 0){
+        background.style.setProperty("--gradient-color","linear-gradient(196deg, #8a9fb2, #5f7991")
+        background.style.setProperty("--gradient-color1","linear-gradient(196deg, #8a9fb2, #5f7991")
+        background.style.setProperty("--gradient-opacity1","1");
+        background.style.setProperty("--gradient-opacity2","0");
+    }
+    else{
+        background.style.setProperty("--gradient-color","linear-gradient(196deg, #8a9fb2, #5f7991")
+        background.style.setProperty("--gradient-color2","linear-gradient(196deg, #8a9fb2, #5f7991")
+        background.style.setProperty("--gradient-opacity2","1");
+        background.style.setProperty("--gradient-opacity1","0");
+    }
+
+})
+
 color4.addEventListener('click',()=>{
     color4.classList.add("selected");
+    color1.classList.remove("selected");
+    color3.classList.remove("selected");
+    color2.classList.remove("selected");
+    color5.classList.remove("selected");
+    color6.classList.remove("selected");
     cartButton.style.background = "#5e89b2";
+
+    chair.src = "assets/chair-4.png";
+    chair.addEventListener("click", startanimation(chair));
 
     let opacity = window.getComputedStyle(
         document.querySelector('#background'), ':before'
@@ -93,10 +162,50 @@ color4.addEventListener('click',()=>{
 
 })
 
+color5.addEventListener('click',()=>{
+    color5.classList.add("selected");
+    color1.classList.remove("selected");
+    color3.classList.remove("selected");
+    color4.classList.remove("selected");
+    color2.classList.remove("selected");
+    color6.classList.remove("selected");
+    cartButton.style.background = "#8c7f76";
+
+    chair.src = "assets/chair-5.png";
+    chair.addEventListener("click", startanimation(chair));
+
+    let opacity = window.getComputedStyle(
+        document.querySelector('#background'), ':before'
+    ).getPropertyValue('opacity');
+    
+    // console.log(opacity)    
+
+    if(opacity == 0){
+        background.style.setProperty("--gradient-color","linear-gradient(196deg, #afa6a0, #8c7f76")
+        background.style.setProperty("--gradient-color1","linear-gradient(196deg, #afa6a0, #8c7f76")
+        background.style.setProperty("--gradient-opacity1","1");
+        background.style.setProperty("--gradient-opacity2","0");
+    }
+    else{
+        background.style.setProperty("--gradient-color","linear-gradient(196deg, #afa6a0, #8c7f76")
+        background.style.setProperty("--gradient-color2","linear-gradient(196deg, #afa6a0, #8c7f76")
+        background.style.setProperty("--gradient-opacity2","1");
+        background.style.setProperty("--gradient-opacity1","0");
+    }
+
+})
+
 color6.addEventListener('click',()=>{
     color6.classList.add("selected");
+    color1.classList.remove("selected");
+    color3.classList.remove("selected");
+    color4.classList.remove("selected");
+    color5.classList.remove("selected");
+    color2.classList.remove("selected");
     cartButton.style.background = "#5d6160";
 
+    chair.src = "assets/chair-6.png";
+    chair.addEventListener("click", startanimation(chair));
     let opacity = window.getComputedStyle(
         document.querySelector('#background'), ':before'
     ).getPropertyValue('opacity');
